4fe21ac4b021eda06da2c117c5e4f4b079ae7d1a
Anton <me@AntonLydike.de>
2017-10-26 23:42:10 +0200

    fixed indentation for assignement 0

--- assignments/0/addOrMultiply.c
+++ assignments/0/addOrMultiply.c
@@ -1,14 +1,14 @@
 int AddOrMultiply(int number1, int number2)
 {
-  int a;
-  
+	int a;
+
 	if (number1 < 0) {
 		number1 = number1 * (-2);       /*multiply number1 by -2*/
 		a = number1 + number2;          /*add number1 to number2*/
 	} /*endif*/
-	else {
+	else
 		a = number1 * number2;  /* multiply number1 with number2*/
-	}  /*endelse*/
+	                                /*endelse*/
 	a = a + 1;
 	return a;
 }
