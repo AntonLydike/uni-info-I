9fedc785747c73e513b75470d2c35955f4b7d284
Anton <me@AntonLydike.de>
2017-11-14 17:30:31 +0100

    [3] fixed misunderstanding in 12.e

--- assignments/3/12/e.c
+++ assignments/3/12/e.c
@@ -7,15 +7,22 @@ int main(void) {
   printf("Bitte geben Sie eine poisitive, ganze Zahl und einen kleinbuchstaben ein:");
 
   if (
-    scanf("%i%c", &input1, &input2) != 2
-    || getchar() != '\n'
+    scanf("%i", &input1) != 1
     || input1 < 0
-    || input2 < 97
-    || input2 > 122
+    || ((input2 = getchar()) != '\n'
+        && (input2 < 97
+            || input2 > 122
+        )
+    )
   ) {
     printf("Ungültige eingabe!\n");
   } else {
-    printf("Die eingegebenen Zahl ist: %i\nDer eingegebene Buchstabe ist: %c\n", input1, input2);
+    printf("Die eingegebenen Zahl ist: %i\n", input1);
+    if (input2 == 10) {
+      printf("Es wurde kein Buchstabe eingegeben.\n");
+    } else {
+      printf("Der eingegebene Buchstabe ist: %c\n", input2);
+    }
   }
 
   return 0;
