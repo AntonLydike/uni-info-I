0adb1aa96ff1b65be2aa88944071abd2b7f23c85
Anton <me@AntonLydike.de>
2017-11-15 11:18:17 +0100

    [3] fixed misunderstanding in 12.e

--- assignments/3/12/e.c
+++ assignments/3/12/e.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
 int main(void) {
-  int input1;
+  int input1, anz;
   char input2;
 
   printf("Bitte geben Sie eine poisitive, ganze Zahl und einen kleinbuchstaben ein:");
 
-  if (
-    scanf("%i", &input1) != 1
-    || input1 < 0
-    || ((input2 = getchar()) != '\n'
-        && (input2 < 97
-            || input2 > 122
-        )
-    )
+
+  anz = scanf("%i", &input1);
+  input2 = getchar();
+
+  if ( /* Ungültig wenn: */
+    anz == 0                                              /* Keine Zahl 1 gegeben wurde */
+    || input1 < 0                                         /* Die zahl zu klein ist */
+    || (input2 != '\n' && getchar() != '\n')              /* Mehr als ein buchstabe gegeben */
+    || (input2 != '\n' && (input2 < 97 || input2 > 122))  /* Der Buchstabe kein kleinbuchstabe ist */
   ) {
     printf("Ungültige eingabe!\n");
   } else {
     printf("Die eingegebenen Zahl ist: %i\n", input1);
+
+    /* wenn input2 \n ist */
     if (input2 == 10) {
       printf("Es wurde kein Buchstabe eingegeben.\n");
     } else {
